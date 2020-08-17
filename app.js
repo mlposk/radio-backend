05a3be0aa9106597392fd5c70a33b4abeda483dc
Alexander Molokotin <mlposk@yandex.ru>
2020-08-17 19:46:51 +0500

    update routes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var filesRouter = require('./routes/files');
+let indexRouter = require('./routes/index');
+let fileUploadRouter = require('./routes/upload');
+let fileListRouter = require('./routes/files');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/files', filesRouter);
+app.use('/upload', fileUploadRouter);
+app.use('/files', fileListRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
